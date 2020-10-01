3e4e131b88a0bf038b3ecfc5aea1bfd19c4dca5c
Garrett <gflynn@usc.edu>
2020-10-01 10:13:51 -0500

    committed

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
  git push -f git@github.com:garrettmflynn/brainjam.git master:gh-pages
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
