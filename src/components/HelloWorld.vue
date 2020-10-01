d4360c59634986aea796858349955391876de505
Garrett <gflynn@usc.edu>
2020-10-01 08:09:20 -0500

    Add cards.csv

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Welcome to The (Brain) Game from the Future</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Click the cards below to generate your prompt:
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div id="card-holder">
+      <button class="card" style="background: lightskyblue" v-on:click="query('future')">
+        <p>In a </p>
+        <div class="break"/>
+        <div class="blank"><p id='future'>Good</p></div>
+        <div class="break"/>
+        <p>future</p>
+      </button>
+      <button class="card" style="background: lightcoral" v-on:click="query('thing')">
+        <p>There is a</p>
+        <div class="break"/>
+        <div class="blank"><p id='thing'>Dog</p></div>
+        <div class="break"/>
+        <p>...</p>
+      </button>
+      <button class="card" style="background: mediumpurple" v-on:click="query('theme')">
+        <p >Related to</p>
+        <div class="break"/>
+        <div class="blank"><p id='theme'>_______</p></div>
+        <div class="break"/>
+        <p>What is it?</p>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    query: function queryCSV(text_id) {
+      const d3 = require("d3");
+      d3.csv('cards.csv').then(function (data) {
+        data.forEach(function(d) {
+          d[text_id] = +d[text_id];
+        });
+        console.log(data[0]);
+      });
+      console.log(text_id);
+    }
   }
 }
+  // let data = csv.toObjects('@/assets/cards.csv');
+  // let row = Math.floor(Math.random() * length(data));
+  // let choice = data[row][text_id]
+  // document.getElementById(text_id).text(choice);
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -55,4 +73,48 @@ li {
 a {
   color: #42b983;
 }
+
+p{
+  width: 100%;
+}
+
+button{
+  font-family: Montserrat, sans-serif;
+  text-align: center;
+  color: #222222;
+  font-weight: 500;
+}
+
+#card-holder{
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.card{
+  background: antiquewhite;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 25px;
+  height: 250px;
+  width: 200px;
+  padding: 25px 0px;
+  border: 1px solid black;
+}
+
+.break{
+  flex-basis: 100%;
+  height: 0;
+}
+
+  .blank{
+    height: 50px;
+    background: white;
+    flex-grow: 1;
+    display: table-cell;
+    vertical-align: middle;
+    border-top: 2px solid black;
+    border-bottom: 2px solid black;
+  }
+
 </style>
