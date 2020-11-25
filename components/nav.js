56c3a95ddb014f07f388ed6a4fb30620aced7264
Garrett <gflynn@usc.edu>
2020-11-25 10:10:42 -0600

    Moved all app elements into /app directory

--- components/nav.js
+++ components/nav.js
@@ -9,7 +9,7 @@ connectedCallback() {
     <div id="links" class="stretch">
       <a href="/index.html" class="link">Home</a>
 <!--      <a href="about.html" class="link">Our Story</a>-->
-      <a href="/brainstorm.html" class="link">App</a>
+      <a href="/app/brainstorm.html" class="link">App</a>
 <!--      <a href="./games.html" class="link">Games</a>-->
       <a href="/futures-initiative.html" class="link">Futures Initiative</a>
 <!--      <a href="projects.html" class="link">Projects</a>-->
