fcaa8862c3865a089b7fa8b599075673d0d8a5ac
Garrett <gflynn@usc.edu>
2020-11-25 08:58:11 -0600

    Updated directory structure and navlinks

--- components/footer.js
+++ components/footer.js
@@ -23,16 +23,16 @@ connectedCallback() {
     </div>
     <div id="footer-links">
       <div id="footer-links1" class="right-footer">
-        <p class="footer-text"><a class ='link' href="about.html" target="_blank">About</a></p>
+        <p class="footer-text"><a class ='link' href="/about.html" target="_blank">About</a></p>
         <p class="footer-text"><a class ='link' href="mailto:gflynn@usc.edu" target="_blank">Contact</a></p>
       </div>
       <div id="footer-links2" class="right-footer">
-        <p class="footer-text"><a class ='link' href="contribute.html" target="_blank">Contribute</a></p>
-        <p class ='footer-text'><a class ='link' href="rules.html">Rules</a></p>
+        <p class="footer-text"><a class ='link' href="/contribute.html" target="_blank">Contribute</a></p>
+        <p class ='footer-text'><a class ='link' href="/rules.html">Rules</a></p>
       </div>
       <div id="footer-links3" class="right-footer">
-        <p class ='footer-text'><a class ='link' href="terms.html">Terms</a></p>
-        <p class ='footer-text'><a class ='link' href="privacy.html">Privacy</a></p>
+        <p class ='footer-text'><a class ='link' href="/terms.html">Terms</a></p>
+        <p class ='footer-text'><a class ='link' href="/privacy.html">Privacy</a></p>
       </div>
     </div>
     <div class="center-footer">
--- components/nav.js
+++ components/nav.js
@@ -5,13 +5,13 @@ class Nav extends HTMLElement {
 
 connectedCallback() {
     this.innerHTML = `
-      <a href="./index.html" class="logo"><h3>Brains@Play</h3></a>
+      <a href="/index.html" class="logo"><h3>Brains@Play</h3></a>
     <div id="links" class="stretch">
-      <a href="./index.html" class="link">Home</a>
+      <a href="/index.html" class="link">Home</a>
 <!--      <a href="about.html" class="link">Our Story</a>-->
-      <a href="./brainstorm.html" class="link">App</a>
+      <a href="/brainstorm.html" class="link">App</a>
 <!--      <a href="./games.html" class="link">Games</a>-->
-      <a href="./futures.html" class="link">Futures Initiative</a>
+      <a href="/futures-initiative.html" class="link">Futures Initiative</a>
 <!--      <a href="projects.html" class="link">Projects</a>-->
 <!--      <a href="contact.html" class="link">Contact</a>-->
     </div>
