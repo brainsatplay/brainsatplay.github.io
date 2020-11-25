9571018fccb8f94e94324bb8975b4eb7c7541c78
Garrett <gflynn@usc.edu>
2020-11-25 09:08:28 -0600

    Update garrett.js

--- futures/people/garrett.js
+++ futures/people/garrett.js
@@ -7,7 +7,9 @@ class Garrett extends HTMLElement {
         this.innerHTML = `
                     <h3>Garrett Flynn</h3>
                     <i>Initiative Coordinator</i>
-                    <p>Garrett Flynn is a progressive degree student in the Computational Neuroscience (BS) and Media Arts, Games, and Health (MA) programs. Working at the intersection of neuroscience, ethics, and interactive media, he leads several interdisciplinary projects out of the <a href="slab.usc.edu" class="text">Neural Modeling and Interface Lab</a> with <a href="#people" class="text" onclick="getPerson('dong-song')">Dong Song</a>.
+
+                    <p>Garrett Flynn is a neurotechnologist in the Computational Neuroscience (BS) and Media Arts, Games, and Health (MA) programs. Informed by his experiences at the <a href="slab.usc.edu" class="text">Neural Modeling and Interface Lab</a> under the guidance of <a href="#people" class="text" onclick="getPerson('dong-song')">Dr. Dong Song</a>, his work empowers the participatory, human-centered design of next-generation brain devices.
+                    
                 </p>
 `;
     }
