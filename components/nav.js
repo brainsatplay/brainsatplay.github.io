51e41536f6e732a3e6974edf452d2705e1ace5fe
Garrett <gflynn@usc.edu>
2020-12-11 13:57:43 -0600

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -9,7 +9,7 @@ connectedCallback() {
     <div id="links" class="stretch">
       <a href="/" class="link">Home</a>
 <!--      <a href="about.html" class="link">Our Story</a>-->
-      <a href="/sdk/examples/brainstorm" class="link">SDK</a>
+      <a href="/sdk" class="link">SDK</a>
 <!--      <a href="./games.html" class="link">Games</a>-->
       <a href="/futures" class="link">Futures Initiative</a>
 <!--      <a href="projects.html" class="link">Projects</a>-->
