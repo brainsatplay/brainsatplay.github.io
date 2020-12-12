29ef5072e5e7ebd6e66fbde61adde2dccbbc67d8
Garrett <gflynn@usc.edu>
2020-12-12 14:12:04 -0600

    Updated links

--- components/nav.js
+++ components/nav.js
@@ -8,10 +8,10 @@ connectedCallback() {
       <a href="/" class="logo"><h4>Brains@Play</h4></a>
     <div id="links" class="stretch">
       <a href="/" class="link">Home</a>
+      <a href="/futures" class="link">About</a>
 <!--      <a href="about.html" class="link">Our Story</a>-->
       <a href="/sdk" class="link">SDK</a>
 <!--      <a href="./games.html" class="link">Games</a>-->
-      <a href="/futures" class="link">Futures Initiative</a>
 <!--      <a href="projects.html" class="link">Projects</a>-->
 <!--      <a href="contact.html" class="link">Contact</a>-->
     </div>
